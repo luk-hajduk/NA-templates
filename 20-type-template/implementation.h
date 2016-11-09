d27ebf5fd7cdec5cc86966ed88a79161185a3f96
lukasz hajduk <lukasz.hajduk.ext@nokia.com>
2016-11-09 16:04:36 +0000

    complemented implementation.h

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,34 @@
 #pragma once
 
+#include<cstddef>
+#include<utility>
+
 template<typename T>
-class container_wrapper;
+class container_wrapper{
+public:
+container_wrapper() = default;
+container_wrapper(T arg) : value(std::move(arg)){   //:value(arg) - tu jest jedno kopiowanie za duzo
+}
+container_wrapper(const container_wrapper &) = default;//konstruktor kopiujacy
+container_wrapper( container_wrapper &&) = default;//konstruktor przesuwajacy
+container_wrapper & operator = (const container_wrapper &) = default;//operator  kopiujacy
+container_wrapper & operator = ( container_wrapper &&) = default;//operator  przesuwajacy
+
+
+
+
+std::size_t size() const{
+
+return value.size();
+
+}
+
+
+
+private:
+T value;
+
+
+}
+;
 

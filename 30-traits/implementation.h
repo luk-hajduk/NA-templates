1ba3872e8fd88a9ddd874575c80570bb5e2dce6e
lukasz hajduk <lukasz.hajduk.ext@nokia.com>
2016-11-10 12:56:40 +0000

    30-templates reworked

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -1,5 +1,39 @@
+/*#pragma once
+
+#include<type_traits>
+template<typename T, typename T1, typename T2, typename T3>
+struct is_any_of: std::false_type
+{
+};
+template<typename T, typename T2, typename T3>
+struct is_any_of<T, T, T2, T3>: std::true_type
+{
+};
+template<typename T, typename T1, typename T3> ///to cale jest do testu 31 ale teras chcemy to naisac sprytniej zeby bylo ogolniejsze:
+struct is_any_of<T, T1,T, T3>: std::true_type
+{
+};
+template<typename T, typename T1, typename T2>
+struct is_any_of<T, T1, T2, T>: std::true_type
+{
+};
+*/
 #pragma once
 
-template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of;
+#include<type_traits>
+
+template<typename T, typename T1, typename T2, typename T3>//tu tylko dodajemy disjunction ktore robi to co zestaw kilku alternatyw ||-----i 
+struct is_any_of: std::disjunction<std::is_same<T,T1>, std::is_same<T,T2>, std::is_same<T,T3>>
+{
+};
+/*
+#pragma once
+
+#include<type_traits>
+
+template<typename T, typename T1, typename T2, typename T3>//tu tylko dodajemy disjunction ktore robi to co zestaw kilku alternatyw ||-----i 
+struct is_any_of: std::constant_bool::value<std::is_same<T,T1> || std::is_same<T,T2> || std::is_same<T,T3>>
+{
+};
+*/
 

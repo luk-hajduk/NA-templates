5f953a66c44e88cfa452b1f16716a442ea8a66a2
lukasz hajduk <lukasz.hajduk.ext@nokia.com>
2016-11-14 08:43:13 +0000

    fixed <= mistake in inner product

--- 40-algos-impl/inner_product.h
+++ 40-algos-impl/inner_product.h
@@ -9,7 +9,7 @@ T inner_product(InputIt1 first1, InputIt1 last1, InputIt2 first2, T sum,
     BinaryOperation1 op1, BinaryOperation2 op2){
 
 
-    while (first1<=last1) {
+    while (first1!=last1) {
     sum=op1(sum,op2(*first1,*first2));
 
     ++first1;
